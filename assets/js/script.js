98c072f98b9d98d6fd800d24d92d39a3bd78da11
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-03 23:19:24 -0400

    working on 1st question. html should be done. css needs work. js underway

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,50 @@
 var timerEl = document.querySelector("#timer");
 // assign start button to variable
 var startEl = document.querySelector("#start");
+// assign section containers to var
+var containerEl = document.getElementsByClassName("container");
 // assign value to be used in the timer
 var count = 75;
+// Placeholder text until function is called
+timerEl.textContent = 'Time: (Press "Start Quiz")';
 // Define function to be used to start the timer
-// note: timer will not be displayed until user starts the quiz
 function countdown() {
-    if (count > 0) {
-        timerEl.textContent = "Timer: " + count--;
+    if (count > 1) {
+        timerEl.textContent = "Time: " + count-- + " seconds left";
+    }
+    else if (count === 1) {
+        timerEl.textContent = "Time: " + count-- + " second left";
     }
     else {
         clearInterval(countdown);
+        timerEl.textContent = "GAME OVER!"
     }
 }
 // set interval inside another function to be called
 function startTimer() {
-     setInterval(countdown, 1000);
+    setInterval(countdown, 1000);
 }
-// add event listener to call the function when button is clicked
-startEl.addEventListener("click", startTimer)
+// define display style for container
+containerEl[0].style.display = '';
+// add function to hide previous container
+function hideContainer() {
+    containerEl[0].style.display = 'none';
+}
+// add event listener's:
+// Start the timer
+startEl.addEventListener("click", startTimer);
+// Hide the container
+startEl.addEventListener("click", hideContainer);
+// Begin working on second section of html where
+// we will dynamically create content within the
+// id and class placeholders
+// First, put into var and set display to none
+var questionsEl = document.getElementById("questions");
+questionsEl.style.display = 'none';
+// next, create a function to show the questions content
+function showQuestions() {
+    questionsEl.style.display = 'flex';
+}
+// now, add another event listener that will show the new content
+// once the start button is clicked
+startEl.addEventListener("click", showQuestions);
